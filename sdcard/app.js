f237b542c5d7836498e3a0c9695bb322e851c19e
Lorenzo Delana <oss.devel@searchathing.com>
2018-11-11 16:16:54 +0100

    upgrading to new version w/chart

--- sdcard/app.js
+++ sdcard/app.js
@@ -11,10 +11,18 @@ var sensorDesc = [{
     description: "test"
 }];
 
-// set to false in production
+// automatic replaced to debug = false during compilation
+// this is used to debug page index.htm locally
 var debug = true;
 
 //==============================================================================
+
+requirejs.config({
+    "moment": "://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js"
+});
+
+// updated from /info api
+var history_interval_sec = 10;
 
 var baseurl = '';
 if (debug) baseurl = 'http://10.10.4.111';
@@ -27,24 +35,101 @@ function reloadTemp(addr) {
     });
 }
 var reload_enabled = false;
-setInterval(autoreload, 3000);
+setInterval(autoreload, 10000);
 
 function autoreload() {
     if (!reload_enabled) return;
+    reloadall();
+}
+
+async function reloadall() {
     $('.tempdev').each(function (idx) {
         let v = this.innerText;
         console.log('addr=[' + v + ']');
         reloadTemp(v);
+    });
+
+    const res = await $.ajax({
+        url: baseurl + "/temphistory",
+        type: 'GET'
+    });
+
+    var colors = ['orange', 'yellow', 'green', 'blue', 'violet', 'black', 'red'];
+    var ctx = document.getElementById("myChart").getContext('2d');
+
+    var dtnow = moment();
+    console.log('moment now = ' + dtnow.format());
+
+    var i = 0;
+    var dss = [];
+    $.each(res, function (idx, data) {
+        id = Object.keys(data)[0];
+        desc = id;
+        q = $.grep(sensorDesc, (el, idx) => el.id == id);
+        if (q.length > 0) desc = q[0].description;
+
+        if (i > colors.length - 1) color = 'brown';
+        else color = colors[i];
+
+        valcnt = data[id].length;
+
+        dts = [];
+        $.each(data[id], function (idx, val) {            
+            secbefore = (valcnt - idx - 1) * history_interval_sec;            
+            tt = moment(dtnow).subtract(secbefore, 'seconds');            
+            dts.push({
+                t: tt,
+                y: val
+            });
+        });
+
+        dss.push({
+            borderColor: color,
+            label: desc,
+            data: dts,
+            pointRadius: 0
+        });
+
+        ++i;
+    });
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: dss
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    time: {
+                        displayFormats: {
+                            'hour': 'HH:mm'
+                        }
+                    },                    
+                    position: 'bottom'
+                }]
+            }
+        }
     });
 }
 
 async function myfn() {
     // retrieve temperature devices and populate table
 
+    $('.j-spin').removeClass('collapse');
     const res = await $.ajax({
         url: baseurl + '/tempdevices',
         type: 'GET'
     });
+    const resnfo = await $.ajax({
+        url: baseurl + '/info',
+        type: 'GET'
+    });
+    history_interval_sec = resnfo.history_interval_sec;
+    console.log('history_interval_sec = ' + history_interval_sec);
+
+    $('.j-spin').addClass('collapse');
 
     var h = "";
 
@@ -65,13 +150,8 @@ async function myfn() {
         h += "</td>";
 
         // value
-        const restemp = await $.ajax({
-            url: baseurl + '/temp/' + tempId,
-            type: 'GET'
-        });
         h += "<td><span id='t" + tempId + "'>";
-        h += restemp;
-        h += "</span></td>";        
+        h += "</span></td>";
 
         // action
         h += "<td><button class='btn btn-primary' onclick='reloadTemp(\"" + res.tempdevices[i] + "\")'>reload</button></td>";
@@ -79,7 +159,15 @@ async function myfn() {
         h += "</tr>";
     }
 
-    $('#tbody-temp')[0].innerHTML = h;  
+    $('#tbody-temp')[0].innerHTML = h;
+
+    const res2 = await $.ajax({
+        url: baseurl + '/info',
+        type: 'GET'
+    });
+    $('#info')[0].innerHTML = JSON.stringify(res2, null, 2);
+
+    reloadall();
 }
 
 myfn();
--- temp-sensors-sd-card/temp-sensors.ino
+++ temp-sensors-sd-card/temp-sensors.ino
@@ -4,14 +4,18 @@
 //
 //-------------------- PLEASE REVIEW FOLLOW VARIABLES ------------------
 //
-// SECURITY WARNING : set to false ENABLE_CORS in production!
+// SECURITY WARNING : uncomment ENABLE_CORS=0 in production!
 //---------------------
 // I used CORS policy to allow me write index.htm and app.js outside atmega controller from pc
 // using atmega webapis
 //
-#define ENABLE_CORS true
+#define ENABLE_CORS 1
 
-#define MACADDRESS 0x43, 0xcf, 0x8d, 0x9f, 0x5b, 0x89
+// choose one of follow two interface
+//#define USE_ENC28J60
+#define USE_W5500
+
+#define MACADDRESS 0x33, 0xcf, 0x8d, 0x9f, 0x5b, 0x89
 #define MYIPADDR 10, 10, 4, 111
 #define MYIPMASK 255, 255, 255, 0
 #define MYDNS 10, 10, 0, 6
@@ -19,86 +23,99 @@
 #define LISTENPORT 80
 #define MAX_HEADER_SIZE 80
 #define ONE_WIRE_BUS 3
-#define SD_CARD_CS 4
-#define ETH_CS 10
-
 // EDIT DebugMacros to set SERIAL_SPEED and enable/disable DPRINT_SERIAL
+
+#define TEMPERATURE_HISTORY_INTERVAL_SEC 5
+
+#define SD_CARD_CSPIN 4
+#define ETH_CSPIN 10
 
 //
 //==============================================================================
+
+#include <Arduino.h>
 
 // loadtime(ms)/bufsize tests : 5700/1 ; 466/8; 275/16; 162/32; 106/64; 74/128; 62/256
 // bufsize 64 enough
 #define SD_CARD_READ_BUFSIZE 64
 
-#include <Arduino.h>
+#define TEMPERATURE_INTERVAL_MS 5000
+unsigned long lastTemperatureRead, lastTemperatureHistoryRecord;
+
+//-------------------------
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <mywifikey.h>
-
-// enc28j60
-//#include <UIPEthernet.h>
-
-// w5500
-#include <Ethernet.h>
-
-#include <DPrint.h>
-#include <Util.h>
-using namespace SearchAThing::Arduino;
 
 // save 1k flash vs Sd.h
 #include <SdFat.h>
 SdFat SD;
 
-String header;
-EthernetServer server(LISTENPORT);
+#ifdef USE_ENC28J60
+#include <UIPEthernet.h>
+// edit UIPEthernet/utility/uipethernet-conf.h to customize
+// - define UIP_CONF_UDP=0 to reduce flash size
+#endif
 
-#define TEMPERATURE_INTERVAL_MS 5000
+#ifdef USE_W5500
+#include <Ethernet.h>
+#endif
+
+#include <DPrint.h>
+#include <Util.h>
+using namespace SearchAThing::Arduino;
+
+DPrintCls print;
+
+String header;
+EthernetServer server = EthernetServer(LISTENPORT);
+
 #define TEMPERATURE_ADDRESS_BYTES 8
 
 OneWire tempOneWire(ONE_WIRE_BUS);
 DallasTemperature DS18B20(&tempOneWire);
 
 int temperatureDeviceCount = 0;
-float *temperatures = NULL;
+float *temperatures = NULL;    // current temp
 DeviceAddress *tempDevAddress; // DeviceAddress defined as uint8_t[8]
 char **tempDevHexAddress;
 
-// save 800 flash bytes vs standard serial
-DPrintCls print;
+void printFreeram()
+{
+  DPrint(F("Freeram : "));
+  DPrintln((long)FreeMemorySum());
+}
 
 //
 // SETUP
 //
 void setup()
 {
-  pinMode(SD_CARD_CS, OUTPUT);
-  digitalWrite(SD_CARD_CS, HIGH);
+  pinMode(SD_CARD_CSPIN, OUTPUT);
+  digitalWrite(SD_CARD_CSPIN, HIGH);
 
-  pinMode(10, OUTPUT);
-  digitalWrite(10, HIGH);
-
+  pinMode(ETH_CSPIN, OUTPUT);
+  digitalWrite(ETH_CSPIN, HIGH);
   delay(1);
 
+  DPrintln(F("STARTUP"));
+
   DPrintln(F("Init SD"));
-  if (!SD.begin(SD_CARD_CS))
+  if (!SD.begin(SD_CARD_CSPIN))
   {
     DPrintln(F("SD begin failed"));
     while (1)
       ;
   }
 
-  DPrintln(F("SD contents"));
-  SD.ls(&print, LS_R);
 
-  DPrintln(F("initialization done."));
+  lastTemperatureRead = lastTemperatureHistoryRecord = millis();
 
-  DPrintln(F("STARTUP"));
+  printFreeram();
 
   header.reserve(MAX_HEADER_SIZE);
   SetupTemperatureDevices();
-
-  Ethernet.init(ETH_CS);
 
   uint8_t mac[6] = {MACADDRESS};
   uint8_t myIP[4] = {MYIPADDR};
@@ -111,13 +128,6 @@ void setup()
 
   // static
   Ethernet.begin(mac, myIP, myDNS, myGW, myMASK);
-
-  if (Ethernet.hardwareStatus() == EthernetNoHardware)
-  {
-    DPrintln(F("eth hw not found"));
-    while (true)
-      ;
-  }
 
   DPrint("my ip : ");
   for (int i = 0; i < 4; ++i)
@@ -169,17 +179,15 @@ void SetupTemperatureDevices()
       DS18B20.setResolution(12);
     }
   }
+
   ReadTemperatures();
 }
-
-unsigned long lastTemperatureRead;
 
 //
 // TEMPERATURE READ
 //
 void ReadTemperatures()
 {
-  DPrintln(F("reading temperatures"));
   DS18B20.requestTemperatures();
   for (int i = 0; i < temperatureDeviceCount; ++i)
   {
@@ -190,11 +198,14 @@ void ReadTemperatures()
     DPrintln(temp, 4);
     temperatures[i] = temp;
   }
+
   lastTemperatureRead = millis();
 }
 
 #define CCTYPE_HTML 0
 #define CCTYPE_JSON 1
+#define CCTYPE_TEXT 2
+#define CCTYPE_JS 3
 
 void clientOk(EthernetClient &client, int type)
 {
@@ -208,9 +219,20 @@ void clientOk(EthernetClient &client, int type)
   case CCTYPE_JSON:
     client.println("Content-Type: application/json");
     break;
+
+  case CCTYPE_TEXT:
+    client.println("Content-Type: text/plain");
+    break;
+
+  case CCTYPE_JS:
+    client.println("Content-Type: text/javascript");
+    break;
   }
 
+#if ENABLE_CORS == 1
   client.println("Access-Control-Allow-Origin: *");
+#endif
+
   client.println();
 }
 
@@ -226,8 +248,6 @@ void loop()
 
     while ((size = client.available()) > 0)
     {
-      bool foundcmd = false;
-
       header = "";
 
       for (int i = 0; i < min(MAX_HEADER_SIZE, size); ++i)
@@ -241,80 +261,129 @@ void loop()
         header.concat(c);
       }
 
-      DPrint(F("Header ["));
-      DPrint(header.c_str());
-      DPrintln(']');
-
-      foundcmd = false;
-
       //--------------------------
-      // TEMPERATURE
+      // /tempdevices
       //--------------------------
-      if (temperatureDeviceCount > 0)
+      if (strncmp(header.c_str(), "GET /tempdevices", 16) == 0)
       {
-        String q = String("GET /tempdevices");
-        if (header.indexOf(q) >= 0)
+        DPrintln("temp devices");
+        clientOk(client, CCTYPE_JSON);
+
+        client.print(F("{\"tempdevices\":["));
+        for (int i = 0; i < temperatureDeviceCount; ++i)
         {
-          clientOk(client, CCTYPE_JSON);
+          client.print('"');
+          client.print(tempDevHexAddress[i]);
+          client.print('"');
 
-          client.print("{\"tempdevices\":[");
-          for (int i = 0; i < temperatureDeviceCount; ++i)
-          {
-            client.print('"');
-            client.print(tempDevHexAddress[i]);
-            client.print('"');
-
-            client.print(",\"");
-            client.print(tempDevHexAddress[i]);
-            client.print('"');
-
-            if (i != temperatureDeviceCount - 1)
-              client.print(',');
-          }
-          client.print("]}");
-
-          client.stop();
-          break;
+          if (i != temperatureDeviceCount - 1)
+            client.print(',');
         }
+        client.print("]}");
 
-        q = String("GET /temp/");
-        if (header.indexOf(q) >= 0)
-        {
-          bool found = false;
-          if (header.length() - q.length() >= 8)
-          {
-            clientOk(client, CCTYPE_JSON);
-
-            for (int i = 0; i < temperatureDeviceCount; ++i)
-            {
-              if (strncmp(header.c_str() + q.length(), tempDevHexAddress[i],
-                          2 * TEMPERATURE_ADDRESS_BYTES) == 0)
-              {
-                char tmp[20];
-                FloatToString(tmp, temperatures[i], 6);
-
-                client.print(tmp);
-
-                found = true;
-                break;
-              }
-            }
-          }
-          if (!found)
-            client.print(F("not found"));
-
-          client.stop();
-          break;
-        }
+        client.stop();
+        break;
       }
 
       //--------------------------
-      // HELP
+      // /temp/{id}
       //--------------------------
-      if (header.indexOf("GET /") >= 0)
+      if (strncmp(header.c_str(), "GET /temp/", 10) == 0)
       {
-        clientOk(client, CCTYPE_HTML);
+        auto hbasesize = 10; // "GET /temp/"
+        bool found = false;
 
+        if (header.length() - hbasesize >= 8)
+        {
+          clientOk(client, CCTYPE_TEXT);
+
+          for (int i = 0; i < temperatureDeviceCount; ++i)
+          {
+            if (strncmp(header.c_str() + hbasesize, tempDevHexAddress[i],
+                        2 * TEMPERATURE_ADDRESS_BYTES) == 0)
+            {
+              char tmp[20];
+              FloatToString(tmp, temperatures[i], 6);
+
+              client.print(tmp);
+
+              found = true;
+              break;
+            }
+          }
+        }
+        if (!found)
+          client.print(F("not found"));
+
+        client.stop();
+        break;
+      }
+
+      //--------------------------
+      // /temphistory
+      //--------------------------
+      if (strncmp(header.c_str(), "GET /temphistory", 16) == 0)
+      {
+        clientOk(client, CCTYPE_JSON);
+
+        /*
+        DPrint(F("temperatureHistoryFillCnt:"));
+        DPrintln(temperatureHistoryFillCnt);
+        DPrint(F("temperatureHistoryOff:"));
+        DPrintln(temperatureHistoryOff);
+
+        client.print('[');
+        for (int i = 0; i < temperatureDeviceCount; ++i)
+        {
+          client.print(F("{\""));
+          client.print(tempDevHexAddress[i]);
+          client.print(F("\":["));
+          auto j = (temperatureHistoryFillCnt == TEMPERATURE_HISTORY_SIZE) ? temperatureHistoryOff : 0;
+          auto size = min(temperatureHistoryFillCnt, TEMPERATURE_HISTORY_SIZE);
+          for (int k = 0; k < size; ++k)
+          {
+            if (j == TEMPERATURE_HISTORY_SIZE)
+              j = 0;
+            client.print(temperatureHistory[i][j++]);
+            if (k < size - 1)
+              client.print(',');
+          }
+          client.print(F("]}"));
+          if (i != temperatureDeviceCount - 1)
+            client.print(',');
+        }
+        client.print(']');
+*/
+        client.stop();
+        break;
+      }
+
+      //--------------------------
+      // /info
+      //--------------------------
+      if (strncmp(header.c_str(), "GET /info", 9) == 0)
+      {
+        clientOk(client, CCTYPE_JSON);
+
+        client.print('{');
+
+        client.print(F("\"freeram\":"));
+        client.print((long)FreeMemorySum());        
+
+        client.print(F(", \"history_interval_sec\":"));
+        client.print(TEMPERATURE_HISTORY_INTERVAL_SEC);
+
+        client.print('}');
+
+        client.stop();
+        break;
+      }
+
+      //--------------------------
+      // /
+      //--------------------------
+      //if (strncmp(header.c_str(), "GET / ", 6) == 0 || strncmp(header.c_str(), "GET /index.htm", 14) == 0)
+      {
         DPrint(F("Freeram : "));
         DPrintln(FreeMemorySum());
         uint8_t buf[SD_CARD_READ_BUFSIZE];
@@ -340,13 +409,16 @@ void loop()
           pathfilename = j == 0 ? String("index.htm") : String(path);
         }
 
-        DPrint(F("pathfilename ["));
+        DPrint(F("serving ["));
         DPrint(pathfilename.c_str());
         DPrintln(']');
 
         if (SD.exists(pathfilename.c_str()))
         {
-          DPrintln(F("opening"));
+          if (pathfilename.indexOf(".js") >= 0)
+            clientOk(client, CCTYPE_JS);
+          else
+            clientOk(client, CCTYPE_HTML);
 
           auto f = SD.open(pathfilename.c_str());
           while (f.available())
@@ -362,35 +434,38 @@ void loop()
           DPrint(pathfilename.c_str());
           DPrintln(']');
         }
+
+        client.stop();
+
+        break;
       }
-      client.stop();
     }
   }
-  else if (TimeDiff(lastTemperatureRead, millis()) > TEMPERATURE_INTERVAL_MS)
+
+  if (TimeDiff(lastTemperatureRead, millis()) > TEMPERATURE_INTERVAL_MS)
   {
-    DPrint(F("Freeram : "));
-    DPrintln(FreeMemorySum());
+    printFreeram();
+    ReadTemperatures();
+  }
 
-    auto link = Ethernet.linkStatus();
-    DPrint(F("Link status: "));
-    switch (link)
-    {
-    case Unknown:
-      DPrintln(F("Unknown"));
-      break;
-    case LinkON:
-      DPrintln(F("ON"));
-      break;
-    case LinkOFF:
-      DPrintln(F("OFF"));
-      break;
-    }
-
+  if (TimeDiff(lastTemperatureHistoryRecord, millis()) > 1000UL * TEMPERATURE_HISTORY_INTERVAL_SEC)
+  {
     DPrintln(F("SD contents"));
     SD.ls(&print, LS_R);
 
-    ReadTemperatures();
+    /*
+    if (temperatureHistoryFillCnt < TEMPERATURE_HISTORY_SIZE)
+      ++temperatureHistoryFillCnt;
 
-    delay(1000);
+    if (temperatureHistoryOff == TEMPERATURE_HISTORY_SIZE)
+      temperatureHistoryOff = 0;
+
+    for (int i = 0; i < temperatureDeviceCount; ++i)
+    {
+      int8_t t = trunc(round(temperatures[i]));
+      temperatureHistory[i][temperatureHistoryOff] = t;
+    }
+    ++temperatureHistoryOff;*/
+    lastTemperatureHistoryRecord = millis();
   }
 }
